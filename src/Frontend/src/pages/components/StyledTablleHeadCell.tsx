21e5c3e54cf3219b468a8207ecaf77453c988560
flaviocp90 <86709141+flaviocp90@users.noreply.github.com>
2025-07-02 08:36:06 +0000

    Added StyledTableHeadCell component for customizing table headers

---
+++ src/Frontend/src/pages/components/StyledTablleHeadCell.tsx
@@ -0,0 +1,8 @@
+import { styled, TableCell, tableCellClasses } from '@mui/material';
+
+export const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.common.white,
+  },
+}));
